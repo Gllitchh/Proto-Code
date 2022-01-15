db2ad92830062d606c404447801db555131a8204
Gllitchh <79875708+Gllitchh@users.noreply.github.com>
2022-01-14 22:06:32 -0600

    Add files via upload

--- tail_servo.py
+++ tail_servo.py
@@ -3,8 +3,10 @@ import RPi.GPIO as GPIO
 import time
 import sys
 # Set GPIO trig and echo for a range finder.
+
 trig =()# please put in the port that the range finder trig is in.
 echo =()# please put in the port that the range finder echo is in.
+
 # Set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(trig,GPIO.OUT)# this is for the trig. data leaves from this 'port'
@@ -15,12 +17,13 @@ tail_part = 0
 place1 = 0
 place2 = 0
 
-servo_1_port = ()# please put in the numbers of the ports that the servos are in. In each one.
-servo_2_port = ()#servo1 and 2 are the first ones that will be moved. Servo3 and 4 are seccond, and servo 5 and 6 and last to move. 
-servo_3_port = ()#keep this in mind when puting in your ports.
-servo_4_port = ()
-servo_5_port = ()
-servo_6_port = ()
+
+servo_1_port = (11)# please put in the numbers of the ports that the servos are in. In each one.
+servo_2_port = (12)#servo 1 and 2 are the first ones that will be moved. Servo3 and 4 are seccond, and servo 5 and 6 are last to move. 
+servo_3_port = (13)#keep this in mind when puting in your ports.
+servo_4_port = (15)
+servo_5_port = (16)
+servo_6_port = (7)
 
 angle = True
 angle2 = True
@@ -105,21 +108,48 @@ def third_tail_part_servos():#these are to conterol and setup the servos.
     time.sleep(0.1)
     servo1.stop()
     servo2.stop()
-    
-while angle:# puts all the code into a loop that won't stop untilll its set to False
-    
-    while servo_port:
-        
-        servoport = input('Did you configure the ports?')
-        if ('y') in servoport:
-            servo_port = False
+
+def servo_input():
+    global servo_1_port, servo_2_port, servo_3_port, servo_4_port, servo_5_port, servo_6_port
+    servo_port_anser = input('did you put in the ports for the servos? ')
+    if ('y') in servo_port_anser:
+        servo_port_anser =  input('did you put in the ranger finder ports?: ')
+        if ('y') in servo_port_anser:
+            print('ok')
         else:
-            print('nano this file and put in the servo ports please')
-            print('now leaving the program')
-            exit()
+            print('')
+            trig = int(input('ok plaese enter the trig port on the range finder: '))
+            echo = int(input('please enter the echo port on it to: '))
+    else:
+        servo_port_anser = input('do you want to put them in here or no? ')
+        if ('h') in servo_port_anser:
+            print('')
+            print('''servo 1 and 2 are the first ones that will be moved.
+Servo3 and 4 are seccond, and servo 5 and 6 are last to move.
+Please keep this in mind.''')
+            print('')
+            servo_1_port = int(input('ok this is for servo1: '))
+            servo_2_port = int(input('now this is for servo2: '))
+            servo_3_port = int(input('this is for servo3: '))
+            servo_4_port = int(input('please be care full. This is for servo4: '))
+            servo_5_port = int(input('this one is for servo5: '))
+            servo_6_port = int(input('now this is for servo6: '))
+            servo_port_anser = input('did you put in the ranger finder ports?: ')
+            if ('y') in servo_port_anser:
+                print('ok')
+            else:
+                print('')
+                trig = int(input('ok plaese enter the trig port on the range finder: '))
+                echo = int(input('please enter the echo port on it to: '))
             
+            
+    
+servo_input()
+
+while angle:# puts all the code into a loop that won't stop untill its set to False
+        
     while angle2:# I couldn't think of a differnt way to do it :/
-        place = input('which way do you want the tail to move from. Please use 1 as Right to Left and 2 as Left and Right:')
+        place = input('which way do you want the tail to move from. Please use 1 as Right to Left and 2 as Left and Right: ')
         if ('1') in place:# takes user input and puts it into variables 
             place1 = 1
             angle2 = False
@@ -128,11 +158,11 @@ while angle:# puts all the code into a loop that won't stop untilll its set to F
             angle2 = False
         else:
             print('sorry thats not right') # tells you that its not right :|
-
+   
         
-    right_angle = float(input('how much do you want the tail to move to the right between 0 and 90:'))# It's an input for you data
+    right_angle = float(input('how much do you want the tail to move to the right between 0 and 90: '))# It's an input for you data
         
-    left_angle = float(input('how much do you want the tail to move to the left between 0 and 90:'))
+    left_angle = float(input('how much do you want the tail to move to the left between 0 and 90: '))
     
     # Lets the program know were it is in the setup.
     if tail_part ==0:
@@ -141,6 +171,7 @@ while angle:# puts all the code into a loop that won't stop untilll its set to F
         place_first = place1
         place1 = 0
         angle2 = True
+        servo_port = True
         print('ok now doing a preview')
         first_tail_part_servos()
     elif tail_part ==1:
@@ -149,6 +180,7 @@ while angle:# puts all the code into a loop that won't stop untilll its set to F
         place_seccond = place1
         place1 = 0
         angle2 = True
+        servo_port = True
         print('ok now doing a preview')
         seccond_tail_part_servos()
     elif tail_part ==2:
@@ -160,12 +192,15 @@ while angle:# puts all the code into a loop that won't stop untilll its set to F
         third_tail_part_servos()
         angle2 = True
         angle = False
+        servo_port = False
     elif tail_part >=2:
         angle = False
         angle2 = False
-    change = input('whould you like to change the angle or tail movemet?:')
+        servo_port = False
+    change = input('would you like to change the angle or tail movemet?: ')
     if ('y') in change:
         angle = True
+        servo_port = True
         print('ok')
         place1 = 0
         place2 = 0
@@ -173,7 +208,7 @@ while angle:# puts all the code into a loop that won't stop untilll its set to F
         tail_part = tail_part + 1
         
         
-auto = input('are you ready to put it into auto mode?:')
+auto = input('are you ready to put it into auto mode?: ')
 if ('y') in auto:
     print('ok here we go')
     print('runing tests...')
@@ -216,9 +251,10 @@ while auto_move:
         
         
     if distance <=3.5:
-        first_tail_part_servos()# calls them
+        first_tail_part_servos()
         seccond_tail_part_servos()
         third_tail_part_servos()
+            
     else:
         distance = 0
 

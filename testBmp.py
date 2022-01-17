5bd3d90ed38b56e158eaf729033b998cc733d2fc
Gllitchh <79875708+Gllitchh@users.noreply.github.com>
2022-01-16 21:02:15 -0600

    Delete testBmp.py

--- testBmp.py
+++
@@ -1,8 +0,0 @@
-import bmpsensor
-import time
-while True:
-    temp, pressure = bmpsensor.readBmp180()
-    
-    print("Pressure is ",pressure) # Pressure in Pa 
-    print("\n")
-    time.sleep(1)
